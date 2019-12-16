af9c29a923f69bd2a54a2987681ffead86326f87
roger-mahler <roger.mahler@hotmail.com>
2019-12-16 15:01:29 +0100

    Simplified runner configuration

--- runner.py
+++ runner.py
@@ -1,33 +1,35 @@
+import argparse
 import sys
-import os
+import copy
 
 sys.path = sys.path + ['.']
 
-from importer.process import process
+from importer.process import AppService
 
-data_folder = './data'
-
-cmd_opts_isotope = "--host {} --dbname {} --dbuser {} --input-folder {} --output-folder {} --data-filename {} --meta-filename {} --data-types {}".format(
-    "seadserv.humlab.umu.se",
-    "sead_staging_clearinghouse",
-    "clearinghouse_worker",
-    os.path.join(data_folder, "input"),
-    os.path.join(data_folder, "output"),
-    "isotope_data_latest_20191213.xlsm",
-    "metadata_latest_20191104.xlsx",
-    "isotope"
+DEFAULTS_OPTS = argparse.Namespace(
+    data_filename=None,
+    data_types=None,
+    meta_filename="metadata_latest_20191104.xlsx",
+    dbhost='seadserv.humlab.umu.se',
+    dbname='sead_staging',
+    dbuser='clearinghouse_worker',
+    input_folder='./data/input',
+    output_folder='./data/output',
+    port=5432,
+    skip=False,
+    submission_id=None,
+    table_names=None,
+    xml_filename=None
 )
 
-cmd_opts_ceramics = "--host {} --dbname {} --dbuser {} --input-folder {} --output-folder {} --data-filename {} --meta-filename {} --data-types {}".format(
-    "seadserv.humlab.umu.se",
-    "sead_staging",
-    "clearinghouse_worker",
-    os.path.join(data_folder, "input"),
-    os.path.join(data_folder, "output"),
-    "ceramics_data_latest_20191213.xlsx",
-    "metadata_latest_20191104.xlsx",
-    "ceramics"
-)
+def import_file(**kwargs):
 
-process(cmd_opts_ceramics)
+    opts = copy.copy(DEFAULTS_OPTS)
+    opts.__dict__.update(kwargs)
 
+    AppService(opts).process()
+
+#import_file(data_filename='ceramics_data_latest_20191216.xlsx', data_types="Ceramics")
+#import_file(data_filename='dendro_build_data_latest_20191213.xlsm', data_types="Dendro building")
+#import_file(data_filename='dendro_ark_data_latest_20191213.xlsm',   data_types="Dendro archeology")
+import_file(data_filename='isotope_data_latest_20191213.xlsm', data_types="Isotope", xml_filename="./data/output/isotope_data_latest_20191213_20191216-073242_tidy.xml")
