0214925e33fed690ca2ea4c53659b1b707f129c4
Roger Mähler <roger.mahler@hotmail.com>
2024-01-02 13:45:47 +0100

    Refactorings:  - Use ColumnSpec/TableSpec - Use new metadata API for table access - Some renamed variables & minor house cleanings

--- importer/process_xml.py
+++ importer/process_xml.py
@@ -9,7 +9,8 @@ import pandas as pd
 from jinja2 import Environment, Template, select_autoescape
 from loguru import logger
 
-from importer.model import DataImportError, DataTableSpecification, Metadata, SubmissionData
+from importer.model import DataImportError, Metadata, SubmissionData, SubmissionSpecification
+from importer.model.metadata import TableSpec
 
 # pylint: disable=too-many-nested-blocks, too-many-statements
 
@@ -66,33 +67,25 @@ class XmlProcessor:
                 if table_name not in metadata:
                     raise ValueError(f"Table {table_name}: not found in metadata")
 
-                table_spec: dict[str, Any] = metadata[table_name]
+                table_spec: TableSpec = metadata[table_name]
+                data_table: pd.DataFrame = submission.data_tables[table_name]
 
-                referenced_keyset: set[str] = set(submission.get_referenced_keyset(metadata, table_name))
-
-                data_table: str = submission.data_tables[table_name]
-
-                table_namespace: str = "com.sead.database.{}".format(table_spec["java_class"])
+                referenced_keyset: set[str] = submission.get_referenced_keyset(metadata, table_name)
+                table_namespace: str = "com.sead.database.{}".format(table_spec.java_class)
 
                 if data_table is None:
                     continue
 
-                self.emit(
-                    '<{} length="{}">'.format(table_spec["java_class"], data_table.shape[0]),
-                    1,
-                )  # data_table.length
+                self.emit('<{} length="{}">'.format(table_spec.java_class, data_table.shape[0]), 1)  # data_table.length
                 # self.emit_tag(table_specification['java_class'], dict(length=data_table.shape[0]), close=False, indent=1)
 
                 for index, record in data_table.iterrows():
                     try:
                         data_row: dict = record.to_dict()
-                        public_id = data_row[table_spec["pk_name"]] if table_spec["pk_name"] in data_row else np.NAN
+                        public_id: int = data_row[table_spec.pk_name] if table_spec.pk_name in data_row else np.NAN
 
                         if np.isnan(public_id) and np.isnan(data_row["system_id"]):
-                            logger.warning(
-                                "Table %s: Skipping row since both CloneId and SystemID is NULL",
-                                table_name,
-                            )
+                            logger.warning("Table %s: Skipping row since both CloneId and SystemID is NULL", table_name)
                             continue
 
                         system_id = int(data_row["system_id"] if not np.isnan(data_row["system_id"]) else public_id)
@@ -104,130 +97,130 @@ class XmlProcessor:
                         if not np.isnan(public_id):
                             public_id = int(public_id)
                             self.emit('<{} id="{}" clonedId="{}"/>'.format(table_namespace, system_id, public_id), 2)
-                        else:
-                            self.emit('<{} id="{}">'.format(table_namespace, system_id), 2)
+                            continue
 
-                            for column_name, column_spec in table_spec["columns"].items():
-                                if column_name in self.ignore_columns:
-                                    continue
+                        self.emit('<{} id="{}">'.format(table_namespace, system_id), 2)
 
-                                is_fk: bool = column_spec["is_fk"]
-                                is_pk: bool = column_spec["is_pk"]
+                        for column_name, column_spec in table_spec.columns.items():
+                            if column_name in self.ignore_columns:
+                                continue
 
-                                class_name: str = column_spec["class_name"]
+                            class_name: str = column_spec.class_name
 
-                                if column_name not in data_row.keys():
-                                    logger.warning(
-                                        "Table %s, FK column %s: META field name not found in submission",
-                                        table_name,
-                                        column_name,
-                                    )
-                                    continue
+                            if column_name not in data_row.keys():
+                                logger.warning(
+                                    "Table %s, FK column %s: META field name not found in submission",
+                                    table_name,
+                                    column_name,
+                                )
+                                continue
 
-                                camel_case_column_name = self.camel_case_name(column_name)
-                                value = data_row[column_name]
-                                if not is_fk:
-                                    if is_pk:
-                                        value = int(public_id) if not np.isnan(public_id) else system_id
-                                    elif isinstance(value, numbers.Number) and np.isnan(value):
-                                        value = "NULL"
+                            camel_case_column_name: str = self.camel_case_name(column_name)
+                            value = data_row[column_name]
+                            if not column_spec.is_fk:
+                                """The value is a PK or non-FK attribte"""
+                                if column_spec.is_pk:
+                                    value = int(public_id) if not np.isnan(public_id) else system_id
+                                elif isinstance(value, numbers.Number) and np.isnan(value):
+                                    value = "NULL"
+                                else:
+                                    if isinstance(value, str) and any((c in "<>&") for c in value):
+                                        value: str = escape(value)
+
+                                self.emit(
+                                    '<{0} class="{1}">{2}</{0}>'.format(camel_case_column_name, class_name, value),
+                                    3,
+                                )
+
+                            else:
+                                """The value is a FK system_id"""
+                                try:
+                                    fk_table_name: str = metadata[class_name].table_name
+                                    if fk_table_name is None:
+                                        logger.warning(
+                                            "Table %s, FK column %s: unable to resolve FK class %s",
+                                            table_name,
+                                            column_name,
+                                            class_name,
+                                        )
+                                        continue
+
+                                    fk_data_table: str = submission.data_tables[fk_table_name]
+
+                                    if np.isnan(value):
+                                        self.emit(
+                                            '<{} class="com.sead.database.{}" id="NULL"/>'.format(
+                                                camel_case_column_name, class_name
+                                            ),
+                                            3,
+                                        )
+                                        continue
+
+                                    fk_system_id: int = int(value)
+                                    if fk_data_table is None:
+                                        fk_public_id = fk_system_id
                                     else:
-                                        if isinstance(value, str) and any((c in "<>&") for c in value):
-                                            value = escape(value)
-
-                                    self.emit(
-                                        '<{0} class="{1}">{2}</{0}>'.format(camel_case_column_name, class_name, value),
-                                        3,
-                                    )
-
-                                else:  # value is a fk system_id
-                                    try:
-                                        fk_table_name: str = metadata.get_tablename_by_classname(class_name)
-                                        if fk_table_name is None:
+                                        if column_name not in fk_data_table.columns:
                                             logger.warning(
-                                                "Table %s, FK column %s: unable to resolve FK class %s",
+                                                "Table %s, FK column %s: FK column not found in %s, id=%s",
                                                 table_name,
                                                 column_name,
+                                                fk_table_name,
+                                                fk_system_id,
+                                            )
+                                            continue
+                                        fk_data_row = fk_data_table.loc[(fk_data_table.system_id == fk_system_id)]
+                                        if fk_data_row.empty or len(fk_data_row) != 1:
+                                            fk_public_id: int = fk_system_id
+                                        else:
+                                            fk_public_id = fk_data_row[column_name].iloc[0]
+
+                                    class_name = class_name.split(".")[-1]
+
+                                    if np.isnan(fk_public_id):
+                                        self.emit(
+                                            '<{} class="com.sead.database.{}" id="{}"/>'.format(
+                                                camel_case_column_name,
                                                 class_name,
-                                            )
-                                            continue
-
-                                        fk_data_table: str = submission.data_tables[fk_table_name]
-
-                                        if np.isnan(value):
-                                            self.emit(
-                                                '<{} class="com.sead.database.{}" id="NULL"/>'.format(
-                                                    camel_case_column_name, class_name
-                                                ),
-                                                3,
-                                            )
-                                            continue
-
-                                        fk_system_id: int = int(value)
-                                        if fk_data_table is None:
-                                            fk_public_id = fk_system_id
-                                        else:
-                                            if column_name not in fk_data_table.columns:
-                                                logger.warning(
-                                                    "Table %s, FK column %s: FK column not found in %s, id=%s",
-                                                    table_name,
-                                                    column_name,
-                                                    fk_table_name,
-                                                    fk_system_id,
-                                                )
-                                                continue
-                                            fk_data_row = fk_data_table.loc[(fk_data_table.system_id == fk_system_id)]
-                                            if fk_data_row.empty or len(fk_data_row) != 1:
-                                                fk_public_id: int = fk_system_id
-                                            else:
-                                                fk_public_id = fk_data_row[column_name].iloc[0]
-
-                                        class_name = class_name.split(".")[-1]
-
-                                        if np.isnan(fk_public_id):
-                                            self.emit(
-                                                '<{} class="com.sead.database.{}" id="{}"/>'.format(
-                                                    camel_case_column_name,
-                                                    class_name,
-                                                    fk_system_id,
-                                                ),
-                                                3,
-                                            )
-                                        else:
-                                            self.emit(
-                                                '<{} class="com.sead.database.{}" id="{}" clonedId="{}"/>'.format(
-                                                    camel_case_column_name,
-                                                    class_name,
-                                                    int(fk_system_id),
-                                                    int(fk_public_id),
-                                                ),
-                                                3,
-                                            )
-
-                                    except:
-                                        logger.error(
-                                            "Table %s, id=%s, process failed for column %s",
-                                            table_name,
-                                            system_id,
-                                            column_name,
+                                                fk_system_id,
+                                            ),
+                                            3,
                                         )
-                                        raise
+                                    else:
+                                        self.emit(
+                                            '<{} class="com.sead.database.{}" id="{}" clonedId="{}"/>'.format(
+                                                camel_case_column_name,
+                                                class_name,
+                                                int(fk_system_id),
+                                                int(fk_public_id),
+                                            ),
+                                            3,
+                                        )
 
-                            # ClonedId tag is always emitted (NULL id missing)
-                            self.emit(
-                                '<clonedId class="java.util.Integer">{}</clonedId>'.format(
-                                    "NULL" if np.isnan(public_id) else int(public_id)
-                                ),
-                                3,
-                            )
-                            self.emit(
-                                '<dateUpdated class="java.util.Date">{}</dateUpdated>'.format(date_updated),
-                                3,
-                            )
-                            self.emit("</{}>".format(table_namespace), 2)
+                                except:
+                                    logger.error(
+                                        "Table %s, id=%s, process failed for column %s",
+                                        table_name,
+                                        system_id,
+                                        column_name,
+                                    )
+                                    raise
 
-                            if 0 < max_rows < index:
-                                break
+                        # ClonedId tag is always emitted (NULL id missing)
+                        self.emit(
+                            '<clonedId class="java.util.Integer">{}</clonedId>'.format(
+                                "NULL" if np.isnan(public_id) else int(public_id)
+                            ),
+                            3,
+                        )
+                        self.emit_date_updated(
+                            date_updated,
+                            3,
+                        )
+                        self.emit("</{}>".format(table_namespace), 2)
+
+                        if 0 < max_rows < index:
+                            break
 
                     except Exception as x:
                         logger.error("CRITICAL FAILURE: Table %s %s", table_name, x)
@@ -239,17 +232,21 @@ class XmlProcessor:
                         table_name,
                         len(referenced_keyset),
                     )
-                    class_name = metadata[table_name]["java_class"]
                     for key in referenced_keyset:
                         self.emit(
-                            '<com.sead.database.{} id="{}" clonedId="{}"/>'.format(class_name, int(key), int(key)),
+                            '<com.sead.database.{} id="{}" clonedId="{}"/>'.format(
+                                table_spec.java_class, int(key), int(key)
+                            ),
                             2,
                         )
-                self.emit("</{}>".format(table_spec["java_class"]), 1)
+                self.emit("</{}>".format(table_spec.java_class), 1)
 
             except:
                 logger.exception("CRITICAL ERROR")
                 raise
+
+    def emit_date_updated(self, date_updated: str, indent: int) -> None:
+        self.emit('<dateUpdated class="java.util.Date">{}</dateUpdated>'.format(date_updated), indent)
 
     def process_lookups(self, metadata: Metadata, submission: SubmissionData, table_names: list[str]) -> None:
         template_str = """
@@ -262,13 +259,13 @@ class XmlProcessor:
         template: Template = self.jinja_env.from_string(template_str)
 
         for table_name in table_names:
-            referenced_keyset: set[str] = set(submission.get_referenced_keyset(metadata, table_name))
+            referenced_keyset: set[str] = submission.get_referenced_keyset(metadata, table_name)
 
             if len(referenced_keyset) == 0:
                 logger.info("Skipping %s: not referenced", table_name)
                 continue
 
-            class_name: str = metadata[table_name]["java_class"]
+            class_name: str = metadata[table_name].java_class
             xml: str = template.render(
                 lookups=referenced_keyset,
                 class_name=class_name,
@@ -283,7 +280,7 @@ class XmlProcessor:
         table_names: list[str] = None,
         extra_names: list[str] = None,
     ) -> None:
-        specification: DataTableSpecification = DataTableSpecification(
+        specification: SubmissionSpecification = SubmissionSpecification(
             metadata=metadata, ignore_columns=self.ignore_columns
         )
 
@@ -307,9 +304,9 @@ class XmlProcessor:
 
             raise DataImportError("Process ABORTED since submission does not conform to SPECIFICATION")
 
-        tables_to_process: list[str] = submission.index_tablenames if table_names is None else table_names
+        tables_to_process: list[str] = submission.index_table_names if table_names is None else table_names
         extra_names: set[str] = (
-            set(metadata.sead_schema.keys()) - set(submission.data_tablenames) if extra_names is None else extra_names
+            set(metadata.sead_schema.keys()) - set(submission.data_table_names) if extra_names is None else extra_names
         )
 
         self.emit('<?xml version="1.0" ?>')
