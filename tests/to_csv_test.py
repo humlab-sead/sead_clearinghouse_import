ae0bb1752f7c9e1b266bf02ec6fabb1a08c067a6
Roger Mähler <roger.mahler@hotmail.com>
2024-01-17 10:45:16 +0100

    Refactor XML parsing tests in to_csv_test.py

--- tests/to_csv_test.py
+++ tests/to_csv_test.py
@@ -4,7 +4,6 @@ from importer.to_csv import (
     RecordValue,
     Table,
     xml_to_columns,
-    xml_to_csv,
     xml_to_record_values,
     xml_to_records,
     xml_to_tables,
@@ -25,7 +24,7 @@ XML_SNIPPET: str = """<?xml version="1.0" ?>
 <TblTaxaTreeMaster length="1">
  <com.sead.database.TblTaxaTreeMaster id="18197" clonedId="18197"/>
 </TblTaxaTreeMaster>
-  <TblAbundances length="2">
+<TblAbundances length="2">
     <com.sead.database.TblAbundances id="3930">
       <abundanceId class="java.lang.Integer">3930</abundanceId>
       <taxonId class="com.sead.database.TblTaxaTreeMaster" id="18197" clonedId="18197"/>
@@ -50,24 +49,39 @@ XML_SNIPPET: str = """<?xml version="1.0" ?>
 
 
 def test_xml_to_records_by_str():
+
+    """Test that tables are correctly extracted from XML string"""
+    tables: list[Table] = list(xml_to_tables(XML_SNIPPET))
+    assert len(tables) == 4, "No tables found"
+    assert { x.table_type for x in tables } == {'TblAbundanceElements', 'TblBiblio', 'TblTaxaTreeMaster', 'TblAbundances'}
+    assert Table('TblAbundanceElements', '1') in tables, "Table not found"
+    assert Table('TblBiblio', '2') in tables, "Table not found"
+    assert Table('TblTaxaTreeMaster', '1') in tables, "Table not found"
+    assert Table('TblAbundances', '2') in tables, "Table not found"
+
+    """Test that columns are correctly extracted from XML string"""
+    columns: list[Column] = list(xml_to_columns(XML_SNIPPET))
+    assert len(columns) == 7
+    assert { x.column_name for x in columns } == {'abundanceId', 'taxonId', 'analysisEntityId', 'abundanceElementId', 'abundance', 'clonedId', 'dateUpdated'}
+    assert Column('TblAbundances', 'abundanceId', 'java.lang.Integer') in columns, "Column not found"
+    assert Column('TblAbundances', 'taxonId', 'com.sead.database.TblTaxaTreeMaster') in columns, "Column not found"
+        
+
+    """Test that records are correctly extracted from XML string"""
     records: list[Record] = list(xml_to_records(XML_SNIPPET))
-    abundances = [r for r in records if r.class_name == 'TblAbundances']
+    abundances: list[Record] = [r for r in records if r.class_name == 'TblAbundances']
     assert len(abundances) == 2
     assert {r.public_id for r in abundances} == {'NULL', '12'}, "Incorrect public IDs found"
 
-    references = [r for r in records if r.class_name == 'TblBiblio']
+    references: list[Record] = [r for r in records if r.class_name == 'TblBiblio']
     assert len(references) == 2
     assert Record('TblBiblio', '352', '352') in references
     assert Record('TblBiblio', '129', '129') in references
     assert Record('TblAbundances', '3930', 'NULL') in abundances
     assert Record('TblAbundances', '3931', '12') in abundances
 
-    references = [r for r in records if r.class_name == 'TblBiblio']
-    assert len(references) == 2
-    assert Record('TblBiblio', '352', '352') in references
-    assert Record('TblBiblio', '129', '129') in references
-
-    record_values: list[RecordValue] = list(xml_to_records(XML_SNIPPET))
+    """Test that record values are correctly extracted from XML string"""
+    record_values: list[RecordValue] = list(xml_to_record_values(XML_SNIPPET))
     assert len(record_values) > 0, "No records found"
     assert len(record_values) == 14
     assert (
@@ -88,12 +102,25 @@ def test_xml_to_records_by_str():
             'analysisEntityId',
             'com.sead.database.TblAnalysisEntities',
             '4191',
-            '4191',
+            'NULL',
             'NULL',
         )
         in record_values
     )
-
+    assert (
+        RecordValue(
+            'TblAbundances',
+            '3931',
+            'NULL',
+            'clonedId',
+            'java.util.Integer',
+            'NULL',
+            'NULL',
+            '12',
+        )
+        in record_values
+    )
+    # RecordValue(class_name='TblAbundances', system_id='3931', public_id='NULL', column_name='clonedId', column_type='java.util.Integer', fk_system_id='NULL', fk_public_id='NULL', column_value='12')
 
 def test_xml_to_records_by_file():
     test_xml_path: str = 'tests/test_data/building_dendro_reduced.xml'
