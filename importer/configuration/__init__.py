bc33ee8b4de65b46df007c0ec5ba09ecd3ae7601
Roger Mähler <roger.mahler@hotmail.com>
2024-11-28 13:41:53 +0100

    refactor: deprecate .env

--- importer/configuration/__init__.py
+++ importer/configuration/__init__.py
@@ -5,11 +5,3 @@ import dotenv
 
 from .config import Config
 from .inject import ConfigStore, ConfigValue, configure_context, inject_config
-
-
-def dburi_from_env() -> str:
-    """
-    Returns the database URI from the environment variables.
-    """
-    dotenv.load_dotenv(".env")
-    return f"postgresql://{os.environ['DBUSER']}@{os.environ['DBHOST']}:{os.environ.get('DBPORT', '5432')}/{os.environ['DBNAME']}"
