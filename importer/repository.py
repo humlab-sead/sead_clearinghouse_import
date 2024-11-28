928e796775e04e07cd7f296c1e62a4a10d330ccb
Roger Mähler <roger.mahler@hotmail.com>
2024-11-28 13:36:14 +0100

    refactor: consolidated uploades

--- importer/repository.py
+++ importer/repository.py
@@ -1,96 +1,17 @@
-import abc
-import io
-import os
-from typing import Any, Type
+from typing import Any
 
 import psycopg2
 from loguru import logger
 from psycopg2.extensions import connection as Connection
 
-from .to_csv import xml_to_csv_to_db
+from .uploader.xml_uploader import BaseUploader, Uploaders
 from .utility import log_decorator
 
 
-class BaseUploader(abc.ABC):
-    @abc.abstractmethod
-    def upload(self, connection: Connection, xml_filename: str | Any, submission_id: int) -> None:
-        pass
-
-    @abc.abstractmethod
-    def extract(self, connection: Connection, submission_id: int) -> None:
-        pass
-
-
-class XmlUploader(BaseUploader):
-    def __init__(self, *, target_schema: str = "clearing_house") -> None:
-        self.target_schema: str = target_schema
-
-    @log_decorator(enter_message=" ---> uploading XML...", exit_message=" ---> XML uploaded")
-    def upload(self, connection: Connection, xml_filename: str | Any, submission_id: int) -> None:
-        if xml_filename is None:
-            raise ValueError("Either xml or filename must be provided")
-
-        if not isinstance(xml_filename, str):
-            raise ValueError("XML must be a string or a filename")
-
-        if len(xml_filename) < 200:
-            if not os.path.exists(xml_filename):
-                raise ValueError("XML file does not exist")
-            with io.open(xml_filename, mode="r", encoding="utf-8") as f:
-                xml: str = f.read()
-
-        with connection.cursor() as cursor:
-            sql = """ update clearing_house.tbl_clearinghouse_submissions set xml = %s where submission_id = %s; """
-            cursor.execute(sql, (xml, submission_id))
-        connection.commit()
-
-    @log_decorator(enter_message=" ---> extracting submission...", exit_message=" ---> submission extracted")
-    def extract(self, connection: Connection, submission_id: int) -> None:
-        """Extract submission into staging tables."""
-        with connection.cursor() as cursor:
-            logger.info("   --> extracting table names from xml...")
-            cursor.callproc("clearing_house.fn_extract_and_store_submission_tables", (submission_id,))
-
-            logger.info("   --> extracting columns from xml...")
-            cursor.callproc("clearing_house.fn_extract_and_store_submission_columns", (submission_id,))
-
-            logger.info("   --> extracting records from xml...")
-            cursor.callproc("clearing_house.fn_extract_and_store_submission_records", (submission_id,))
-
-            logger.info("   --> extracting values from xml...")
-            cursor.callproc("clearing_house.fn_extract_and_store_submission_values", (submission_id,))
-
-
-class CsvUploader(BaseUploader):
-    def __init__(self, *, csv_folder: str = "./csv_files", target_schema: str = "clearing_house") -> None:
-        self.csv_folder: str = csv_folder
-        self.target_schema: str = target_schema
-
-    @log_decorator(enter_message=" ---> uploading CSV submission...", exit_message=" ---> CSV submission uploaded")
-    def upload(
-        self,
-        connection: Connection,
-        xml_filename: str | Any,
-        submission_id: int,  # pylint: disable=unused-argument
-    ) -> None:
-        xml_to_csv_to_db(connection, xml_filename, self.csv_folder, self.target_schema)
-
-    @log_decorator(enter_message=" ---> extracting submission...", exit_message=" ---> submission extracted")
-    def extract(self, connection: Connection, submission_id: int) -> None:
-        """Extract submission into staging tables."""
-        with connection.cursor() as cursor:
-            cursor.callproc("clearing_house.fn_extract_csv_upload_to_staging_tables", (submission_id,))
-
-
-UPLOADERS: dict[str, Type[BaseUploader]] = {"xml": XmlUploader, "csv": CsvUploader}
-
-
 class SubmissionRepository:
-    def __init__(self, db_options: dict[str, str], uploader: BaseUploader = None) -> None:
+    def __init__(self, db_options: dict[str, str], uploader: str | BaseUploader = None) -> None:
         self.db_options: dict[str, str] = db_options
-        self.uploader: BaseUploader | None = (
-            uploader if uploader is BaseUploader else UPLOADERS.get(uploader, XmlUploader)()
-        )
+        self.uploader: BaseUploader | None = uploader if uploader is BaseUploader else Uploaders.get(uploader)()
         self.connection: Connection | None = None
         self.timeout_seconds: int = 300
 
