10864c74f73bb3f91cb2cb717e20a4a5d5d946d8
Roger Mähler <roger.mahler@hotmail.com>
2024-11-28 13:41:17 +0100

    refactor:: deprecated global data

--- tests/__init__.py
+++
@@ -1,3 +0,0 @@
-SOURCE_EXCEL_FILENAME: str = "data/input/building_dendro_2023-12_import.xlsx"
-REDUCED_EXCEL_FILENAME: str = "tests/test_data/building_dendro_reduced.xlsx"
-TEST_SITES: list[int] = [1635]
