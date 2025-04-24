7696034cf06bc412bedf548dba50982b0bea59ae
Roger Mähler <roger.mahler@hotmail.com>
2025-04-24 11:02:36 +0200

    refactor: update database connection handling in CSV import script

--- importer/scripts/csv2table.py
+++ importer/scripts/csv2table.py
@@ -4,6 +4,8 @@ import click
 import pandas as pd
 from loguru import logger
 
+from sqlalchemy.engine import URL, create_engine
+
 # pylint: disable=no-value-for-parameter,unused-argument,too-many-positional-arguments
 
 
@@ -19,7 +21,7 @@ from loguru import logger
 @click.option("--database", "-d", "dbname", type=str, default=None, help="Target database name.")
 @click.option("--host", "-h", "host", type=str, help="Target database server")
 @click.option("--user", "-u", "user", type=str, help="Database user")
-@click.option("--port", "-p", "port", type=int, default=5432, help="Server port number.")
+@click.option("--port", "-p", "port", type=str, default="5433", help="Server port number.")
 @click.option("--overwrite/--no-overwrite", type=bool, is_flag=True, default=False, help="Overwrite existing data if exists.")
 @click.pass_context
 def import_csv(
@@ -38,9 +40,18 @@ def import_csv(
     Imports a CSV file into a table in the database using Pandas.
     """
 
-    data: pd.DataFrame = pd.read_csv(filename, sep=delimiter, quoting=quoting)
+    url: URL = URL.create(
+        drivername="postgresql",
+        username=user,
+        host=host,
+        port=port,
+        database=dbname,
+    )
 
-    data.to_sql(name=tablename, con=f"postgresql://{user}@{host}:{port}/{dbname}", if_exists='replace' if overwrite else 'fail', index=False)
+    logger.info(f"Connecting to database '{url}'")
+
+    data: pd.DataFrame = pd.read_csv(filename, sep=delimiter, quoting=quoting)
+    data.to_sql(name=tablename, con=create_engine(url), if_exists='replace' if overwrite else 'fail', index=False)
     
     logger.info(f"Data imported successfully into table '{tablename}' in database '{dbname}'")
 
